f1ac433fa2cbb44e358546640565cf06403a6c73
Fantazindy <paulo.pete.k.razon@gmail.com>
2025-01-02 10:47:09 +0800

    added VideoServiceUnitTests

---
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace TestNinja.UnitTests.Mocking
+{
+	[TestFixture]
+	public class VideoServiceTests
+	{
+		[Test]
+		public void ReadVideoTitle_FileIsEmpty_ReturnErrorMessage()
+		{
+			var videoService = new VideoService();
+
+			var result = videoService.ReadVideoTitle(new FakeFileReader());
+
+			Assert.That(result, Does.Contain("error").IgnoreCase);
+		}
+	}
+}
+
