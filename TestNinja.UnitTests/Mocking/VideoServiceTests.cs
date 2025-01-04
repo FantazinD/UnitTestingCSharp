8ba579057c0194aa746c19d5705737dd53779a1d
Fantazindy <paulo.pete.k.razon@gmail.com>
2025-01-04 12:23:08 +0800

    completed VideoService unit tests

--- TestNinja.UnitTests/Mocking/VideoServiceTests.cs
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -8,12 +8,14 @@ namespace TestNinja.UnitTests.Mocking
 	{
 		private VideoService _videoService;
 		private Mock<IFileReader> _fileReader;
+		private Mock<IVideoRepository> _videoRepository;
 
 		[SetUp]
 		public void SetUp()
 		{
             _fileReader = new Mock<IFileReader>();
-            _videoService = new VideoService(_fileReader.Object);
+			_videoRepository = new Mock<IVideoRepository>();
+            _videoService = new VideoService(_fileReader.Object, _videoRepository.Object);
         }
 
 		[Test]
@@ -24,6 +26,32 @@ namespace TestNinja.UnitTests.Mocking
 			var result = _videoService.ReadVideoTitle();
 
 			Assert.That(result, Does.Contain("error").IgnoreCase);
+		}
+
+		[Test]
+		public void GetUnprocessedVideosAsCsv_AllVideosAreProcessed_ReturnEmptyString(){
+			_videoRepository.Setup(vr => vr.GetUnprocessedVideos()).Returns(new List<Video>());
+			var result = _videoService.GetUnprocessedVideosAsCsv();
+
+			Assert.That(result, Is.EqualTo(""));
+		}
+
+		[Test]
+		public void GetUnprocessedVideosAsCsv_SomeVideosAreUnprocessed_ReturnStringWithUnprocessedVideoIds(){
+			_videoRepository.Setup(vr => vr.GetUnprocessedVideos()).Returns(new List<Video>(){
+				new Video(){
+					Id = 1
+				},
+				new Video(){
+					Id = 2
+				},
+				new Video(){
+					Id = 3
+				}
+			});
+			var result = _videoService.GetUnprocessedVideosAsCsv();
+
+			Assert.That(result, Is.EqualTo("1,2,3"));
 		}
 	}
 }
