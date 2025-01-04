2285e5d3558797f93b7c3b71f74d97c517208b19
Fantazindy <paulo.pete.k.razon@gmail.com>
2025-01-04 15:40:29 +0800

    refactor BookingHelper towards loosely-coupled design

---
+++ TestNinja/Mocking/BookingRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace TestNinja.Mocking
+{
+	public interface IBookingRepository
+	{
+		IQueryable<Booking> GetActiveBookings(int? excludedBookingId = null);
+    }
+
+	public class BookingRepository
+	{
+		public BookingRepository()
+		{
+		}
+
+		public IQueryable<Booking> GetActiveBookings(int? excludedBookingId = null)
+		{
+            var unitOfWork = new UnitOfWork();
+            var bookings =
+                unitOfWork.Query<Booking>()
+                    .Where(
+                        b => b.Status != "Cancelled");
+
+			if (excludedBookingId.HasValue)
+				bookings = bookings.Where(b => b.Id != excludedBookingId.Value);
+
+			return bookings;
+        }
+	}
+}
+
