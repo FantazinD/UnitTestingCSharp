ac29e0f6d365478719c8c1191d943a02fd365359
Fantazindy <paulo.pete.k.razon@gmail.com>
2025-01-04 13:27:42 +0800

    UnitTests for EmployeController DeleteEmploye Method

---
+++ TestNinja.UnitTests/Mocking/EmployeeControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Moq;
+
+namespace TestNinja.UnitTests.Mocking
+{
+	[TestFixture]
+	public class EmployeeControllerTests
+	{
+		private Mock<IEmployeeStorage> _employeeStorage;
+		private EmployeeController _employeeController;
+
+		public EmployeeControllerTests()
+		{
+		}
+
+		[SetUp]
+		public void SetUp()
+		{
+			_employeeStorage = new Mock<IEmployeeStorage>();
+			_employeeController = new EmployeeController(_employeeStorage.Object);
+		}
+
+		[Test]
+		public void DeleteEmployee_WhenCalled_DeleteEmployeeFromDb()
+		{
+            _employeeController.DeleteEmployee(1);
+
+            _employeeStorage.Verify(es => es.DeleteEmployee(1));
+		}
+
+		[Test]
+		public void DeleteEmployee_WhenCalled_ReturnsRedirectResult()
+		{
+            var result = _employeeController.DeleteEmployee(1);
+
+            Assert.That(result, Is.TypeOf<RedirectResult>());
+        }
+
+    }
+}
+
