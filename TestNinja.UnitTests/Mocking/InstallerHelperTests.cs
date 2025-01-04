525c429f0f8b0f8d2734b80b4f52987a4a3b62e2
Fantazindy <paulo.pete.k.razon@gmail.com>
2025-01-04 13:01:47 +0800

    UnitTests for InstallerHelper

---
+++ TestNinja.UnitTests/Mocking/InstallerHelperTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using Moq;
+
+namespace TestNinja.UnitTests.Mocking
+{
+	[TestFixture]
+	public class InstallerHelperTests
+	{
+		private InstallerHelper _installerHelper;
+		private Mock<IFileDownloader> _fileDownloader;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_fileDownloader = new Mock<IFileDownloader>();
+			_installerHelper = new InstallerHelper(_fileDownloader.Object);
+		}
+
+		[Test]
+		public void DownloadInstaller_DownloadFails_ReturnFalse()
+		{
+			_fileDownloader.Setup(fd => fd.DownloadFile(It.IsAny<string>(), It.IsAny<string>())).Throws<WebException>();
+			var result = _installerHelper.DownloadInstaller(customerName: "a", installerName: "b");
+
+            Assert.That(result, Is.False);
+		}
+
+		[Test]
+		public void DownloadInstaller_DownloadSucceeds_ReturnTrue()
+		{
+			var result = _installerHelper.DownloadInstaller(customerName: "a", installerName: "b");
+
+            Assert.That(result, Is.True);
+		}
+	}
+}
+
---
+++ TestNinja/Mocking/FileDownloader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+
+namespace TestNinja.Mocking
+{
+	public interface IFileDownloader
+	{
+		void DownloadFile(string url, string destinationPath);
+	}
+
+	public class FileDownloader : IFileDownloader
+	{
+		public FileDownloader()
+		{
+		}
+
+        public void DownloadFile(string url, string destinationPath)
+        {
+            var client = new WebClient();
+
+			client.DownloadFile(url, destinationPath);
+        }
+    }
+}
+
